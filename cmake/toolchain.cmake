a304b967ef3b8a3c4e63a83f3881584b45bf1073
asarium <asarium@gmail.com>
2014-02-05 19:41:02 +0100

    Add -std=c++11 for clang

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,4 +1,6 @@
 if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
 	include(toolchain-gcc)
+elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+	include(toolchain-clang)
 ENDIF(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
 	
